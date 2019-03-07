2b109e11547586ab5f0e2e9c38991462545fad5b
Daniel Danner <daniel@danner.de>
2019-03-07 18:44:10 +0100

    Make tests run sequentially (fix flaky coverage?)

--- ci-build.py
+++ ci-build.py
@@ -76,7 +76,7 @@ if args.run_tests:
     tox_env.update({
         'PWD': build_dir,
         })
-    sp.check_call(f'tox -e py37 -- -n auto -v -x', shell=True, cwd=build_dir, env=tox_env)
+    sp.check_call(f'tox -e py37 -- -n 1 -v -x', shell=True, cwd=build_dir, env=tox_env)
 
     sp.check_call('lcov --capture --directory . --output-file coverage.info', shell=True, cwd=build_dir)
     sp.check_call('lcov --remove coverage.info "/usr/*" --output-file coverage.info', shell=True, cwd=build_dir)
